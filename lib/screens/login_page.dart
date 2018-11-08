04cfdb12ecce8888c1ada81f06e73d318c0cf32f
mario.corzo <mario.corzo@etermax.com>
2018-11-08 14:39:04 -0300

    Add some login widgets.

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -25,4 +25,57 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+
+  Widget _logo() {
+    return new Hero(
+        tag: 'hero',
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/flutter-icon.png'),
+        )
+    );
+  }
+
+  Widget _sizedBox(_height) {
+    return new SizedBox(
+      height: _height,
+    );
+  }
+
+  Widget _emailInput() {
+    String _email;
+    return new TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: new InputDecoration(
+          hintText: 'Email',
+          icon: new Icon(
+            Icons.mail,
+            color: Colors.grey,
+          )),
+      validator: (value) => value.isEmpty ? 'Email can \'t be empty' : null,
+      onSaved: (value) => _email = value,
+    );
+  }
+
+  Widget _passwordInput() {
+    String _password;
+    return new TextFormField(
+      obscureText: true,
+      autofocus: false,
+      decoration: new InputDecoration(
+          hintText: 'Password',
+          icon: new Icon(
+            Icons.lock,
+            color: Colors.grey,
+          )),
+      validator: (value) => value.isEmpty ? 'Password can \'t be empty' : null,
+      onSaved: (value) => _password = value,
+    );
+  }
+
+
 }
+enum FormMode { SIGNIN, SIGNUP }
+
